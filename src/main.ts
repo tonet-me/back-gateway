5115b38b90845286cea50e8e8038e186624a4079
MasoodKeshvari <keshvari.developer@gmail.com>
2021-07-23 16:24:55 +0430

    config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const URL = `${process.env.BASE_URL}:${process.env.PORT}`;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: URL,
+        package: 'gateway',
+        protoPath: path.join(__dirname, 'proto/user.proto'),
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
