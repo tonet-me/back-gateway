9d805bd71886bd35c81f8362c1ad3ed3c74ed57b
MasoodKeshvari <keshvari.developer@gmail.com>
2021-11-25 21:09:21 +0330

    fix

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -80,23 +80,23 @@ export class CardController {
     );
   }
 
-  @Put('/basic-info/:cardId')
-  @UseGuards(UserStatusGuard)
-  @UserStatus(UserStatusEnum.COMPLETED)
-  @UseGuards(AuthGuard)
-  updateBasicInfoCard(
-    @Req() req: IReq,
-    @Param() { cardId }: CardIdDTO,
-    @Body() updateBasicInfoCardBody: UpdateBasicInfoCardDto,
-  ): Observable<IResponse<ICard>> {
-    return from(
-      this.cardService.updateBasicInfoCard({
-        ...updateBasicInfoCardBody,
-        _id: cardId,
-        userId: req.user._id,
-      }),
-    );
-  }
+  // @Put('/basic-info/:cardId')
+  // @UseGuards(UserStatusGuard)
+  // @UserStatus(UserStatusEnum.COMPLETED)
+  // @UseGuards(AuthGuard)
+  // updateBasicInfoCard(
+  //   @Req() req: IReq,
+  //   @Param() { cardId }: CardIdDTO,
+  //   @Body() updateBasicInfoCardBody: UpdateBasicInfoCardDto,
+  // ): Observable<IResponse<ICard>> {
+  //   return from(
+  //     this.cardService.updateBasicInfoCard({
+  //       ...updateBasicInfoCardBody,
+  //       _id: cardId,
+  //       userId: req.user._id,
+  //     }),
+  //   );
+  // }
 
   @Put('/:cardId')
   @UseGuards(UserStatusGuard)
