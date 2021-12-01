0629a448c38c21bf77032e6254b18cf2605f57c1
MasoodKeshvari <keshvari.developer@gmail.com>
2021-12-02 00:31:34 +0330

    fix enum

--- src/card/enum/socail.title.dto.ts
+++ src/card/enum/socail.title.dto.ts
@@ -1,8 +1,9 @@
 export enum SocialTitleEnum {
-  INSTAGRAM = 'instagram',
-  TWEETER = 'tweeter',
-  YOUTUBE = 'youtube',
-  WHATSAPP = 'whatsApp',
-  TELEGRAM = 'telegram',
-  LINKEDIN = 'linkedin',
+  INSTAGRAM = 'Instagram',
+  TWEETER = 'Tweeter',
+  YOUTUBE = 'Youtube',
+  WHATSAPP = 'WhatsApp',
+  TELEGRAM = 'Telegram',
+  LINKEDIN = 'Linkedin',
+  FACEBOOK = 'Facebook',
 }
